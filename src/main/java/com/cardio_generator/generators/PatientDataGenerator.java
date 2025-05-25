58e4a0138f904351029b6b0be901e9f091e2be33
valterse <79447676+valterse@users.noreply.github.com>
2025-05-25 18:13:46 +0200

    updat

--- src/main/java/com/cardio_generator/generators/PatientDataGenerator.java
+++
@@ -1,7 +0,0 @@
-package com.cardio_generator.generators;
-
-import com.cardio_generator.outputs.OutputStrategy;
-
-public interface PatientDataGenerator {
-    void generate(int patientId, OutputStrategy outputStrategy);
-}
--- src/main/java/com/cardio_generator/outputs/OutputStrategy.java
+++
@@ -1,5 +0,0 @@
-package com.cardio_generator.outputs;
-
-public interface OutputStrategy {
-    void output(int patientId, long timestamp, String label, String data);
-}
